95ae56d677f2a3841342b927ab3da420931c6316
mangement alhamshary <alhamsharymangement@gmail.com>
2025-06-28 20:21:49 +0000

    still the same error when refresh the page

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // <--- Add this line for static export
   typescript: {
     ignoreBuildErrors: true,
     
