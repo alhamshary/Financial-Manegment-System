6b683f9050848d85da7004b6bbf07ad6a6a4229b
alhamshary <alhamsharymangement@gmail.com>
2025-07-02 15:33:37 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default function LoginPage() {
 
   console.log("user HOM",user)
   useEffect(() => {
-    if (!loading && user) {
+    if ( user) {
       router.push("/dashboard");
     }
   }, [user, loading, router]);
 
-  if(loading || user) {
+  if(loading ) {
     return (
       <div className="flex h-screen w-full items-center justify-center">
         <div className="h-12 w-12 animate-spin rounded-full border-4 border-primary border-t-transparent"></div>
