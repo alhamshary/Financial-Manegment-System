ad7f8029180181a953058b011acbcffc6ca825fd
alhamshary <alhamsharymangement@gmail.com>
2025-07-04 14:31:48 +0300

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const { user, loading } = useAuth();
   const router = useRouter();
 
-  console.log("user HOME", user)
+  console.log("user HOME test", user)
   useEffect(() => {
     if (user) {
       router.push("/dashboard");
